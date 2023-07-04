43c89ae645f7e13b497ea075c3552876ac30da43
xanekka <xanekka@gmail.com>
2023-07-03 23:25:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import streamlit as st
-import streamlit_ext as ste
+import streamlit_ext as set
+from transformers import pipeline
 
-from datetime import time, datetime, date
+st.title('Sentiment Analyser App')
+st.write('This app uses the Hugging Face Transformers [sentiment analyser](https://huggingface.co/course/chapter1/3?fw=tf) library to clasify the sentiment of your input as postive or negative. The web app is built using [Streamlit](https://docs.streamlit.io/en/stable/getting_started.html).')
+st.write(
+    'To find out how this app was developed, please check out my [Medium post](https://medium.com/@rtkilian/deploy-and-share-your-sentiment-analysis-app-using-streamlit-sharing-2ba3ca6a3ead). To see my source code, have a look at my [GitHub repo](https://github.com/rtkilian/streamlit-huggingface).')
 
-option = ste.selectbox(
-    "How would you like to be contacted?",
-    range(100),
-    key="selectbox",
-)
+st.write('*Note: it will take up to 30 seconds to run the app.*')
 
-st.write("You selected:", option)
+form = ste.form(key='sentiment-form')
+user_input = form.text_area('Enter your text')
+submit = form.form_submit_button('Submit')
 
-d = ste.date_input("When's your birthday", date(2019, 7, 6), key="date_input")
-st.write("Your birthday is:", d)
+if submit:
+    classifier = pipeline("sentiment-analysis")
+    result = classifier(user_input)[0]
+    label = result['label']
+    score = result['score']
 
-t = ste.time_input("Set an alarm for", time(8, 45), key="time_input")
-st.write("Alarm is set for", t)
+    if label == 'POSITIVE':
+        st.success(f'{label} sentiment (score: {score})')
+    else:
+        st.error(f'{label} sentiment (score: {score})')
