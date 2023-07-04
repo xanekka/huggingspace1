0310f175c3e60e18b846a822457ff9ac613b1220
xanekka <xanekka@gmail.com>
2023-07-03 23:04:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 import streamlit as st
 from transformers import pipeline
 
-st.title('Milestone 2: Sentiment Analyser')
-st.write('*Note: it can take up to 30 seconds to run the app.*')
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
 
-form = st.form(key='sentiment-form')
-with form:
-    st.write("Inside the form")
-    user_input = form.text_area('Enter your text')
-    submit = form.form_submit_button('Submit')
-    if submit:
-        classifier = pipeline("sentiment-analysis")
-        result = classifier(user_input)[0]
-        label = result['label']
-        score = result['score']
-        if label == 'POSITIVE':
-            st.success(f'{label} sentiment (score: {score})')
-        else:
-            st.error(f'{label} sentiment (score: {score})')
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
